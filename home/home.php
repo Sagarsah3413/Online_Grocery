4df00215cb27fc110e217c323d8073d2afc0ba37
Kshitij-cel <kshitijkanth2000@gmail.com>
2020-08-15 21:34:37 +0530

    products

---
+++ home/home.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>homepage</title>
+</head>
+<body>
+    
+
+    <div id="product">
+        <h3>products</h3>
+        <h2>groceries</h2>
+        <div id="grid">
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+        </div>
+
+        <h2>fruits</h2>
+        <div id="grid">
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+        </div>
+
+        <h2>vegetables</h2>
+        <div id="grid">
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+
+            <div id="items">    
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+            <div id="items">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laudantium error. Dolor explicabo ipsum sit, provident laborum incidunt delectus amet. Ut facere quis vero hic explicabo, ex unde neque accusantium!</p>
+            
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
