9e8deff3974a11c47f5268f39846569e5262ab00
Quasilius-Starlord <joysen833@gmail.com>
2020-08-16 20:18:56 +0530

    sidemenu

--- home/home.php
+++ home/home.php
@@ -4,56 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="../stylesheet/header.css">
     <title>homepage</title>
 </head>
 <body>
-    <div id="product">
-        <h3>products</h3>
-        <h2>groceries</h2>
-        <div id="grid">
-            <?php
-            for ($i=0; $i < 5; $i++) { 
-                echo "<div id='items'> 
-                            <div>
-                            <img src='images/down.png' alt='product pic'>
-                            </div>
-                            <h4>product</h4>
-                            <p>Rs 130 <span>140</span></p>
-                        </div>";
-            }
-            ?>
-        </div>
+    <?php require_once'../shared/header.php'; ?>
+    <div class="container">
+        <nav>
+            <div>home</div>
+            <div>groceries</div>
+            <div>fruits</div>
+            <div>vegetables</div>
+        </nav>
+        <div id="product">
+            <section></section>
+            <h3>products</h3>
+            <h2>groceries</h2>
+            <div id="grid">
+                <?php
+                for ($i=0; $i < 5; $i++) {
+                    echo "<div id='items'> 
+                                <div>
+                                <img src='images/down.png' alt='product pic'>
+                                </div>
+                                <h4>product</h4>
+                                <p>Rs 130 <span>140</span></p>
+                            </div>";
+                }
+                ?>
+            </div>
 
-        <h2>fruits</h2>
-        <div id="grid">
-            <?php
-            for ($i=0; $i < 5; $i++) { 
-                echo "<div id='items'> 
-                            <div>
-                            <img src='images/down.png' alt='product pic'>
-                            </div>
-                            <h4>product</h4>
-                            <p>Rs 130 <span>140</span></p>
-                        </div>";
-            }
-            ?>
-        </div>
+            <h2>fruits</h2>
+            <div id="grid">
+                <?php
+                for ($i=0; $i < 5; $i++) { 
+                    echo "<div id='items'> 
+                                <div>
+                                <img src='images/down.png' alt='product pic'>
+                                </div>
+                                <h4>product</h4>
+                                <p>Rs 130 <span>140</span></p>
+                            </div>";
+                }
+                ?>
+            </div>
 
-        <h2>vegetables</h2>
-        <div id="grid">
-            <?php
-            for ($i=0; $i < 5; $i++) { 
-                echo "<div id='items'> 
-                            <div>
-                            <img src='images/down.png' alt='product pic'>
-                            </div>
-                            <h4>product</h4>
-                            <p>Rs 130 <span>140</span></p>
-                        </div>";
-            }
-            ?>
-        </div>
+            <h2>vegetables</h2>
+            <div id="grid">
+                <?php
+                for ($i=0; $i < 5; $i++) { 
+                    echo "<div id='items'> 
+                                <div>
+                                <img src='images/down.png' alt='product pic'>
+                                </div>
+                                <h4>product</h4>
+                                <p>Rs 130 <span>140</span></p>
+                            </div>";
+                }
+                ?>
+            </div>
+        </>
+
     </div>
+    
 </body>
 
 </html>
---
+++ shared/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div>price<span id="price" class="sight"></span></div>
+    <div>cart <span id="cart" class="sight"></span></div>
+    <div>buy now</div>
+</header>
