540783f28bdcad7282d15ffaa37c37291fee5dd0
Kshitij-cel <kshitijkanth2000@gmail.com>
2020-08-16 20:01:50 +0530

    cart icon added

--- home/home.php
+++ home/home.php
@@ -14,227 +14,408 @@
         <h2>groceries</h2>
         <div id="grid">
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
 
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
         </div>
 
         <h2>fruits</h2>
         <div id="grid">
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
 
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
         </div>
 
         <h2>vegetables</h2>
         <div id="grid">
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
 
             <div id="items">    
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
             <div id="items">
-                <div>
+                <div id="img">
                     <img src="images/down.png" alt="product pic">
                 </div>
-                <h4>product</h4>
-                <p>Rs 130 <span>140</span></p>
+                <div id="details">
+                    <h4>product</h4>
+                    <p>Rs 130 <span>140</span></p>
+                    <div id="cart">
+                        <img src="images/cart.png" alt="cart">
+                        <p> Add To Cart</p> 
+                    </div>
+                </div>
             </div>
         </div>
 
-        
+</body>
+</html>       
         
