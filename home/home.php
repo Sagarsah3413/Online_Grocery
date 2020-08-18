ac357561cd347041e6b29d970fe86c271e8d6ca8
Kshitij-cel <kshitijkanth2000@gmail.com>
2020-08-18 14:25:10 +0530

    fresh start

--- home/home.php
+++ home/home.php
@@ -37,72 +37,7 @@
                     </div>
                 </div>
             </section>
-            <h3>products</h3>
-            <h2>groceries</h2>
-            <div class="grid">
-                <?php
-                $query = "SELECT * FROM (SELECT * FROM `grocers`.`products` where type='" . $type[0] . "' ORDER BY `productid` DESC LIMIT $offset,5) sub ORDER BY `productid` ASC;";
-                $data = $dbconnection -> query($query);
-        
-                while($row = $data -> fetch_assoc()) {
-                    ?>
-                <div class='items'> 
-                    <div class="img">
-                        <img src='images/down.png' alt='product pic' >
-                    </div>
-                    <h4><?php echo $row['product name']; ?></h4>
-                    <p>Rs <?php echo $row['price']; ?> <span class="cross">Rs <?php echo $row['mprice']; ?></span></p>
-                    <div class='cart' id="<?php echo $row['productid'] ?>">
-                        <article><span class="control"><img src="images/minus.png" alt=""></span> Quantity <span class="quantity">0</span><span class="control"><img src="images/plus.png" alt=""></span></article>
-                        <p><img src='images/cart.png' alt='cart'> <span class="addcart">Add To Cart</span></p>
-                    </div>
-                </div>
-                <?php } ?>
-            </div>
-
-            <h2>fruits</h2>
-            <div class="grid">
-                <?php
-                $query = "SELECT * FROM (SELECT * FROM `grocers`.`products` where type='" . $type[1] . "' ORDER BY `productid` DESC LIMIT $offset,5) sub ORDER BY `productid` ASC;";
-                $data = $dbconnection -> query($query);
-        
-                while($row = $data -> fetch_assoc()) {
-                    ?>
-                <div class='items'> 
-                    <div class="img">
-                        <img src='images/down.png' alt='product pic'>
-                    </div>
-                    <h4><?php echo $row['product name'] ?></h4>
-                    <p>Rs <?php echo $row['price'] ?> <span class="cross">Rs <?php echo $row['mprice'] ?></span></p>
-                    <div class='cart' id="<?php echo $row['productid'] ?>">
-                        <article><span class="control"><img src="images/minus.png" alt=""></span> Quantity <span class="quantity">0</span><span class="control"><img src="images/plus.png" alt=""></span></article>
-                        <p><img src='images/cart.png' alt='cart'> <span class="addcart">Add To Cart</span></p> 
-                    </div>
-                </div>
-                <?php } ?>
-            </div>
-
-            <h2>vegetables</h2>
-            <div class="grid">
-                <?php
-                $query = "SELECT * FROM (SELECT * FROM `grocers`.`products` where type='" . $type[2] . "' ORDER BY `productid` DESC LIMIT $offset,5) sub ORDER BY `productid` ASC;";
-                $data = $dbconnection -> query($query);
-        
-                while($row = $data -> fetch_assoc()) {
-                    ?>
-                <div class='items'>
-                    <div class="img">
-                        <img src='images/down.png' alt='product pic'>
-                    </div>
-                    <h4><?php echo $row['product name']; ?></h4>
-                    <p>Rs <?php echo $row['price']; ?> <span class="cross">Rs <?php echo $row['mprice']; ?></span></p>
-                    <div class='cart' id="<?php echo $row['productid'] ?>">
-                        <article><span class="control"><img src="images/minus.png" alt=""></span>Quantity<span class="quantity">0</span><span class="control"><img src="images/plus.png" alt=""></span></article>
-                        <p><img src='images/cart.png' alt='cart'> <span class="addcart">Add To Cart</span></p> 
-                    </div>
-                </div>
-                <?php } ?>
-            </div>
+            
         </div>
 
     </div>
