200f86744e543ec84d4fd2be2774a924e27755da
Ratan206thk <Ratan206thk@gmail.com>
2020-08-20 22:37:02 +0545

    'xxx'

--- cart/cart.js
+++ cart/cart.js
@@ -7,7 +7,7 @@ for(let i=0;i<prop.length;i++)
 {
     var str=document.createElement('tr');
     str.innerHTML="<td><img src='../home/images/down.png' alt=''><span>"+fav[prop[i]].name+
-    "</span></td><td><button class='minus'>-</button><input type='text' value='"+fav[prop[i]].quantity+
+    "</span></td><td><button class='minus'>-</button><input type='number' min='0' value='"+fav[prop[i]].quantity+
     "'><button class='plus'>+</button></td><td>Rs."+fav[prop[i]].rate+"</td><td>Rs."+
     fav[prop[i]].quantity*fav[prop[i]].rate+"</td>";
     car.append(str);
@@ -29,17 +29,33 @@ let input;
 // console.log(minus);
 
 for (let i = 0; i < plus.length; i++) {
-
+    input = plus[i].parentElement.children[1];
+    input.addEventListener('input',()=>{
+        input = plus[i].parentElement.children[1];
+        fav[prop[i]].quantity=input.value;
+        sessionStorage.setItem("fav", JSON.stringify(fav));
+    });
     plus[i].addEventListener('click', () => {
         input = plus[i].parentElement.children[1];
         input.value++;
+        fav[prop[i]].quantity=input.value;
+        sessionStorage.setItem("fav", JSON.stringify(fav));
     });
 
     minus[i].addEventListener('click', () => {
         input = plus[i].parentElement.children[1];
-        if (input.value > 0)
+        if (input.value > 1)
+        {
             input.value--;
+            fav[prop[i]].quantity=input.value;
+        }    
+        else {
+            delete fav[prop[i]];
+            car.children[i].style.display="none";
+        }
+        sessionStorage.setItem("fav", JSON.stringify(fav));
     });
+    console.log(fav);
 }
 
 // console.log(sessionStorage.key);
