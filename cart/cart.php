1de9317c2a54e093334c7aaf5c20f00faf7868b1
Quasilius-Starlord <joysen833@gmail.com>
2020-08-21 14:55:54 +0530

    buy more

--- cart/cart.php
+++ cart/cart.php
@@ -19,8 +19,9 @@
     <div id="total">
         <div>Grand Total: Rs.<span>0</span>/- only</div>
     </div>
-    <article>
+    <article id="body">
         <div><a href="<?php echo $sharedfolder; ?>../buy now/buynow.php">Place your Order </a></div>
+        <div><a href="<?php echo $sharedfolder; ?>../home/home.php">Continue Shopping</a></div>
     </article>
     
 </body>
