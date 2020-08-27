006829ed3320224e57a352afd4bcbe04fda82b6e
Ratan206thk <Ratan206thk@gmail.com>
2020-08-27 20:35:44 +0545

    health and care

--- cart/cart.php
+++ cart/cart.php
@@ -29,32 +29,8 @@
         <div><a href="<?php echo $sharedfolder; ?>../home/home.php">Continue Shopping</a></div>
         <div><a href="<?php echo $sharedfolder; ?>../buy now/buynow.php">Place your Order </a></div>
     </article>
-    <div id="image">
-        <i id="prev">
-            <img src="../home/images/img4.png" width="50px" alt="">
-        </i>
-        <i id="next">
-            <img src="../home/images/img5.png" width="50px" alt="">
-        </i>
-        <div id="slider">
-            <div class="img1">
-                <p><a href="../groceries/groceries.php">add to cart</a></p>
-            </div>
-            <div class="img2">
-                <p><a href="../fruits/fruits.php">buy now</a></p>
-            </div>
-            <div id="img3">
-                <p><a href="../vegetables/vegetables.php">free home delievery</a></p>
-            </div>
-            <div class="img1">
-                <p><a href="../groceries/groceries.php">add to cart</a></p>
-            </div>
-            <div class="img2">
-                <p><a href="../fruits/fruits.php">buy now</a></p>
-            </div>
-        </div>
-    </div>  
-    <?php require_once'../shared/footer.php'; ?>  
+    <?php require_once '../shared/slider.php' ?>
+    <?php require_once '../shared/footer.php'; ?>  
 </body>
 <script src="cart.js" defer></script>
 <script src="../shared/ajax.js" defer></script>
