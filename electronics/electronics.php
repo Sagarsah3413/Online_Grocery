4bff3e8f05dec90743d5af9ec1f76e1d8ab4d8ac
Ratan206thk <Ratan206thk@gmail.com>
2020-09-28 00:27:08 +0545

    njs

--- electronics/electronics.php
+++ electronics/electronics.php
@@ -16,7 +16,21 @@ $sharedfolder='../shared/';
 <body>
     <?php require_once'../shared/header.php'; ?>
     <div class="container">
-        <div class="child" id="slider"></div>
+        <div id="image">
+            <i id="prev">
+                <img src="<?php echo $sharedfolder; ?>../home/images/prev.png" width="50px" alt="">
+            </i>
+            <i id="next">
+                <img src="<?php echo $sharedfolder; ?>../home/images/next.png" width="50px" alt="">
+            </i>
+            <div id="slider">
+                <img src="<?php echo $sharedfolder; ?>../home/images/snacks.png" alt="">
+                <img src="<?php echo $sharedfolder; ?>../home/images/groc2.jpeg" alt="">
+                <img src="<?php echo $sharedfolder; ?>../home/images/groc3.jpeg" alt="">
+                <img src="<?php echo $sharedfolder; ?>../home/images/snacks.png" alt="">
+                <img src="<?php echo $sharedfolder; ?>../home/images/groc2.jpeg" alt="">
+            </div>
+        </div>
         <div class="child" id="name">
             <h1 id="title">Title of product</h1>
             <div id="price"><span class="rate">Rs.40000</span><span class="cross">Rs.30000</span></div>
