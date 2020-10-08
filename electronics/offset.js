620d95175752022cc07244f1940e5a2e5cca79c5
Quasilius-Starlord <joysen833@gmail.com>
2020-10-09 00:09:48 +0530

    jhbjh

--- electronics/offset.js
+++ electronics/offset.js
@@ -7,7 +7,6 @@ let spread = []
 
 
 for (let i = 0; i < clips.length; i++) {
-
     clips[i].style.display = 'inline-block';
     ratio = clips[i].naturalWidth / clips[i].naturalHeight;
     padding = ratio * frameheight;
@@ -16,29 +15,29 @@ for (let i = 0; i < clips.length; i++) {
     clips[i].style.width = `${padding}px`;
     spread.push(padding);
 }
-console.log(spread)
 
 for (let i = 0; i < clips.length; i++) {
     padding = (width - spread[i]) / 2;
+    console.log(clips[0].style.width, clips[0].clientWidth)
     clips[i].style.padding = `0px ${padding}px`;
 };
 
-function setimagewidth(i) {
-    padding = (width - spread[i]) / 2;
-    clips[i].style.padding = `0px ${padding}px`;
-}
+// function setimagewidth(i) {
+//     padding = (width - spread[i]) / 2;
+//     clips[i].style.padding = `0px ${padding}px`;
+// }
 
-window.addEventListener('resize', () => {
-    frameheight = document.getElementById('slider').clientHeight;
+// window.addEventListener('resize', () => {
+//     frameheight = document.getElementById('slider').clientHeight;
 
-    for (let i = 0; i < clips.length; i++) {
-        ratio = clips[i].naturalWidth / clips[i].naturalHeight;
-        padding = ratio * frameheight;
-        if (padding > width)
-            padding = width;
-        clips[i].style.width = `${padding}px`;
-        spread.push(padding);
-        clips[i].style.padding = `0px`;
-        setimagewidth(i);
-    }
-});
+//     for (let i = 0; i < clips.length; i++) {
+//         ratio = clips[i].naturalWidth / clips[i].naturalHeight;
+//         padding = ratio * frameheight;
+//         if (padding > width)
+//             padding = width;
+//         clips[i].style.width = `${padding}px`;
+//         spread.push(padding);
+//         clips[i].style.padding = `0px`;
+//         setimagewidth(i);
+//     }
+// });
